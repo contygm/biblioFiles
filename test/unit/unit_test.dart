b98324b861035d35f83e2bd78638723272b09c6f
contygm <contygm@gmail.com>
2020-06-28 21:35:22 -0700

    test_automation: unit tests run locally

---
+++ test/unit/unit_test.dart
@@ -0,0 +1,13 @@
+import 'package:test/test.dart';
+
+void main() {
+  group('Counter', () {
+    test('value should start at 0', () {
+      expect(0, 0);
+    });
+
+    test('value should be example', () {
+      expect('example', 'example');
+    });
+  });
+}
