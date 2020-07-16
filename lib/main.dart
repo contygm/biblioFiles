c50243f2fde55c2c58a9e2f1ba6fecd49a3acdc6
contygm <contygm@gmail.com>
2020-07-15 21:35:31 -0700

    pr_tests: ios auth works

--- lib/main.dart
+++ lib/main.dart
@@ -16,19 +16,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -38,15 +26,6 @@ class App extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -62,12 +41,12 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
         actions: <Widget>[
-          Builder(builder: (BuildContext context) {
+          Builder(builder: (context) {
             return FlatButton(
               child: const Text('Sign out'),
               textColor: Theme.of(context).buttonColor,
               onPressed: () async {
-                final FirebaseUser user = await _auth.currentUser();
+                final user = await _auth.currentUser();
                 if (user == null) {
                   Scaffold.of(context).showSnackBar(const SnackBar(
                     content: Text('No one has signed in.'),
@@ -75,16 +54,16 @@ class _MyHomePageState extends State<MyHomePage> {
                   return;
                 }
                 _signOut();
-                final String uid = user.uid;
+                final uid = user.uid;
                 Scaffold.of(context).showSnackBar(SnackBar(
-                  content: Text(uid + ' has successfully signed out.'),
+                  content: Text('$uid has successfully signed out.'),
                 ));
               },
             );
           })
         ],
       ),
-      body: Builder(builder: (BuildContext context) {
+      body: Builder(builder: (context) {
         return ListView(
           scrollDirection: Axis.vertical,
           children: <Widget>[
@@ -130,7 +109,7 @@ class _GoogleSignInSectionState extends State<_GoogleSignInSection> {
             _success == null
                 ? ''
                 : (_success
-                    ? 'Successfully signed in, uid: ' + _userID
+                    ? 'Successfully signed in, uid: $_userID'
                     : 'Sign in failed'),
             style: TextStyle(color: Colors.red),
           ),
@@ -140,21 +119,20 @@ class _GoogleSignInSectionState extends State<_GoogleSignInSection> {
   }
 
   void _signInWithGoogle() async {
-    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
-    final GoogleSignInAuthentication googleAuth =
-        await googleUser.authentication;
-    final AuthCredential credential = GoogleAuthProvider.getCredential(
+    final googleUser = await _googleSignIn.signIn();
+    final googleAuth = await googleUser.authentication;
+    final credential = GoogleAuthProvider.getCredential(
       accessToken: googleAuth.accessToken,
       idToken: googleAuth.idToken,
     );
-    final FirebaseUser user =
+    final user =
         (await _auth.signInWithCredential(credential)).user;
     assert(user.email != null);
     assert(user.displayName != null);
     assert(!user.isAnonymous);
     assert(await user.getIdToken() != null);
 
-    final FirebaseUser currentUser = await _auth.currentUser();
+    final currentUser = await _auth.currentUser();
     assert(user.uid == currentUser.uid);
     setState(() {
       if (user != null) {
