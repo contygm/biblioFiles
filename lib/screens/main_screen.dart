0e1f7accafb107d548cdc0820a8a553a51ae0f1a
contygm <contygm@gmail.com>
2020-07-18 19:53:33 -0700

    appbar: search UI works, drawer menu works

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key key}) : super(key: key);
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  Icon _searchIcon = Icon(Icons.search);
+  Widget _appBarTitle = Text( 'BiblioFiles' );
+  final TextEditingController _searchController = TextEditingController();
+  String _searchText = "";
+
+  void _searchPressed() {
+    setState(() {
+      if (_searchIcon.icon == Icons.search) {
+        _searchIcon = Icon(Icons.close);
+        _searchText = _searchController.text;
+        _appBarTitle = TextField(
+          controller: _searchController,
+          decoration: InputDecoration(
+            hintText: 'Search...',
+          ),
+          onSubmitted: (value) {
+            _searchText = _searchController.text;
+            print(_searchText);
+          },
+        );
+      } else {
+        _searchController.text = "";
+        _searchIcon = Icon(Icons.search);
+        _appBarTitle = Text('BiblioFiles');
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      endDrawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () {
+                // Navigator.pop(context);
+                print('Clicked SETTINGS');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+              onTap: () {
+                // Navigator.pop(context);
+                print('Clicked PROFILE');
+              },
+            ),
+            Divider(),
+            ListTile(
+              title: Text('Signout'),
+              onTap: () {
+                // Navigator.pop(context);
+                print('Clicked SIGNOUT');
+              },
+            )
+          ],
+        )// Populate the Drawer in the next step.
+      ),
+      appBar: AppBar(
+        title: _appBarTitle,
+        leading: Padding(
+          padding: EdgeInsets.only(left: 6),
+          child: IconButton(
+            icon: _searchIcon,
+            onPressed: () {
+              print('Clicked SEARCH');
+              _searchPressed();
+            },
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Login()
+      )
+    );
+  }
+}
--- test/widget/widget_test.dart
+++ test/widget/widget_test.dart
@@ -1,30 +1,20 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:biblioFiles/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Basic Login smoke test', (tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(App());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Google Sign in'), findsOneWidget);
+    expect(find.text('No one has signed in.'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.text('Sign out'));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Google Sign in'), findsOneWidget);
+    expect(find.text('No one has signed in.'), findsOneWidget);
   });
 }
