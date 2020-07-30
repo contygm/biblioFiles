544a1540b2c8f45e4b4006636f7cf9a45e88735c
wiseja@oregonstate.edu <wiseja@oregonstate.edu>
2020-07-29 18:00:41 -0700

    init

--- lib/screens/add_book_screen.dart
+++ lib/screens/add_book_screen.dart
@@ -2,8 +2,51 @@ import 'package:flutter/material.dart';
 import '../templates/default_template.dart';
 
 class AddBookScreen extends StatelessWidget {
+  static final routes = {'scanBarcode'};
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTemplate(content: Text('Add Book Screen'));
+    return DefaultTemplate(
+        content: Row(children: <Widget>[
+      Column(children: [
+        Text('How do you want to add your Book?'),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          alignment: Alignment.center,
+          child: RaisedButton(
+            onPressed: () async {
+              _callScanBarcode();
+            },
+            child: const Text('Scan Barcode'),
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          alignment: Alignment.center,
+          child: RaisedButton(
+            onPressed: () async {
+              _callIsbnEntry();
+            },
+            child: const Text('ISBN #'),
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          alignment: Alignment.center,
+          child: RaisedButton(
+            onPressed: () async {
+              _callWriteInfo();
+            },
+            child: const Text('Write Info'),
+          ),
+        ),
+      ])
+    ]));
   }
+
+  void _callScanBarcode() async {}
+
+  void _callIsbnEntry() async {}
+
+  void _callWriteInfo() async {}
 }
